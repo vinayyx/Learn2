971705827e195d47a658699e15d9a05d4e4d0a06
vinay kushwah <kushwahvinay95@gmail.com>
2025-08-21 01:36:44 +0530

    frontendurl

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 connectDB()
 
 app.use(cors({
-   origin: "https://learnytix.in", 
+   origin: process.env.FrontendURL, 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
